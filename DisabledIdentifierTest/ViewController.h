553c85d13d54521fb647966f0854d0424307edb2
teejay <tfallon@mail.depaul.edu>
2013-02-12 15:02:57 -0600

    Initial Commit

---
+++ DisabledIdentifierTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DisabledIdentifierTest
+//
+//  Created by teejay on 2/12/13.
+//  Copyright (c) 2013 teejay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DisabledIdentifierTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DisabledIdentifierTest
+//
+//  Created by teejay on 2/12/13.
+//  Copyright (c) 2013 teejay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
